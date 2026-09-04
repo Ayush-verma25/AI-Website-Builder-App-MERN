567f427a1ceeede953e93faf0fab86cde1325845
Ayush Verma <ayve012@gmail.com>
2026-09-04 09:39:53 +0530

    Backednserver Created with Authentication features

---
+++ server/model/User.js
@@ -0,0 +1,31 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+
+const UserSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: { type: String, required: true },
+  },
+  { timestamps: true },
+);
+
+// Hash password before saving to database
+UserSchema.pre("save", async () => {
+  if (!this.isModified("password")) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Compare password method
+UserSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+export const User = mongoose.model("User", UserSchema);
