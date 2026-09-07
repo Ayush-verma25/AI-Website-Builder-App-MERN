15c868b7eef95671592c309edb8d489bdfc1f4f1
Ayush Verma <ayve012@gmail.com>
2026-09-07 19:40:04 +0530

    made changes

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from "react";
 import { useAppContext } from "../context/AppContext";
-import PromptInput from "../components/PromptInput.jsx";
 import { homeTags } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import { ArrowRightIcon, ClockIcon, Trash2Icon } from "lucide-react";
 import moment from "moment";
+import PromptInput from "../components/PromptInput";
 
 const HomePage = () => {
   const navigate = useNavigate();
