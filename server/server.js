b2768b162542584d6fb8648fac637622bc9d6958
Ayush Verma <ayve012@gmail.com>
2026-09-07 19:21:03 +0530

    import problem solve

--- server/server.js
+++ server/server.js
@@ -3,29 +3,29 @@ import "dotenv/config";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { connectToDatabase } from "./config/db.js";
-import authRouter from "./routes/authRoutes.js";
-import projectRouter from "./routes/projectRoutes.js";
+import authRouter from "./Routes/authRoutes.js";
+import projectRouter from "./Routes/projectRoutes.js";
 
 const app = express();
 
-await connectToDatabase()
+await connectToDatabase();
 
-app.use(cors({origin: process.env.ORIGINS.split(","), credentials: true}))
-app.use(cookieParser())
-app.use(express.json())
+app.use(cors({ origin: process.env.ORIGINS.split(","), credentials: true }));
+app.use(cookieParser());
+app.use(express.json());
 
-app.get("/", (req, res)=> res.send("Server is Live!"))
-app.use('/api/auth', authRouter)
-app.use("/api/projects", projectRouter)
+app.get("/", (req, res) => res.send("Server is Live!"));
+app.use("/api/auth", authRouter);
+app.use("/api/projects", projectRouter);
 
-// Centralized error handler 
-app.use((err, _req, res, _next)=>{
-    console.error(`[Error] ${err.message}`);
-    res.status(500).json({error: err.message})
-})
+// Centralized error handler
+app.use((err, _req, res, _next) => {
+  console.error(`[Error] ${err.message}`);
+  res.status(500).json({ error: err.message });
+});
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, ()=>{
-    console.log(`Server is running at http://localhost:${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server is running at ${port}`);
+});
