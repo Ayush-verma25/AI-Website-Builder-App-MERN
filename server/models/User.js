c93ff1aa188426bb1e88b3da33ba4a6d487993cd
Ayush Verma <ayve012@gmail.com>
2026-09-07 22:22:15 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,22 +1,25 @@
-import mongoose, {Schema} from 'mongoose'
-import bcrypt from 'bcrypt'
+import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const UserSchema = new Schema({
-    name: {type: String, required: true},
-    email: { type: String, required: true, unique: true, lowercase: true, trim: true },
+    name: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true, lowercase: true, trim: true, index: true },
     password: { type: String, required: true },
-},{timestamps: true})
+}, { timestamps: true });
 
-// Hash password before saving
-UserSchema.pre('save', async function() {
-    if(!this.isModified('password')) return;
-    const salt = await bcrypt.genSalt(10)
-    this.password = await bcrypt.hash(this.password, salt)
-})
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    try {
+        const salt = await bcrypt.genSalt(12);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
 
-// Compare password method
 UserSchema.methods.comparePassword = async function (password) {
-    return bcrypt.compare(password, this.password)
-}
+    return bcrypt.compare(password, this.password);
+};
 
-export const User = mongoose.model('User', UserSchema)
+export const User = mongoose.model('User', UserSchema);
